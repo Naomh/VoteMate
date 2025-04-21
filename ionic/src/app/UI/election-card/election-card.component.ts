4f76c00fcbc3bce87cc4f70668a7422ba5405d9b
Tomáš Švondr <Noir6027@gmail.com>
2025-04-21 16:14:36 +0200

    fix election card

--- ionic/src/app/UI/election-card/election-card.component.ts
+++ ionic/src/app/UI/election-card/election-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, inject, input, Input, OnInit, Output } from '@angular/core';
 import { Web3Service } from '../../services/web3.service';
 import { HttpService } from '../../services/http.service';
-import { IUser } from '../../services/dexie.service';
+import { DexieService, IUser } from '../../services/dexie.service';
 import { ElectionstagePipe } from '../../pipes/electionstage.pipe';
 import { RouterModule } from '@angular/router';
 import { Address } from 'web3';
@@ -17,56 +17,64 @@ const MainVotingC = require('../../../assets/contracts/MainVotingC.json')
   templateUrl: './election-card.component.html',
   styleUrl: './election-card.component.scss'
 })
-
-export class ElectionCardComponent implements OnInit{
+export class ElectionCardComponent implements OnInit {
+  // #region Inputs and Outputs
   @Input() name!: string;
   @Input() address!: string;
+  @Input() id!: string;
   @Input() user!: IUser | undefined;
   @Input() description: string = "";
+  @Output() preview = new EventEmitter<string>();
+  // #endregion
 
-  @Output() preview = new EventEmitter<string>()
- 
+  // #region Services and Contract
   private web3SVC: Web3Service = inject(Web3Service);
   private httpSVC: HttpService = inject(HttpService);
-  
+  private dexieSVC = inject(DexieService);
   private contract!: any;
-  
+  // #endregion
+
+  // #region State Variables
   protected stage: number = -1;
   protected isEligible!: boolean;
+  // #endregion
 
+  // #region Lifecycle Methods
   async ngOnInit(): Promise<void> {
     this.contract = await this.web3SVC.getSmartContract(this.address, MainVotingC.abi);
     this.isEligible = await this.contract.methods['isVoterEligible'](this.user?.wallet).call();
-  
-    if(this.contract){
-      this.stage = await this.contract.methods.stage().call()
-      console.log(this.isEligible);
-    }
-
+    this.stage = await this.contract.methods.stage().call();
   }
-  public async enroll(button: ButtonComponent){
-    if(!this.user || !this.user.wallet){
+  // #endregion
+
+  // #region Methods
+  protected selectElection() {
+    this.dexieSVC.selectElection(this.id);
+  }
+
+  public async enroll(button: ButtonComponent) {
+    if (!this.user || !this.user.wallet) {
       button.setFailure();
       return;
     }
 
-    
-    try{
+    try {
       button.state = 'loading';
       await this.httpSVC.enrollVoter(this.address, this.user.wallet);
-      button.setSuccess()
-    }catch(e){
+      button.setSuccess();
+    } catch (e) {
       button.setFailure();
     }
     button.state = 'ready';
   }
 
-  public async toPreview(){
+  public async toPreview() {
     this.preview.emit(this.address);
   }
 
-  public async enrollVoters(){
-    this.web3SVC
-    this.httpSVC.enrollVoters(this.address); 
+  public async enrollVoters() {
+    this.web3SVC;
+    this.httpSVC.enrollVoters(this.address);
   }
+  // #endregion
 }
