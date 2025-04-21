78d0490a2f0e8d6df1d614b57dfef119847c0a2a
Tomáš Švondr <Noir6027@gmail.com>
2025-04-21 16:16:28 +0200

    feature reset password

--- ionic/src/app/UI/loginform/loginform.component.ts
+++ ionic/src/app/UI/loginform/loginform.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from "../button/button.component";
+import { SnackbarService } from '../../services/snackbar.service';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 export interface ILoginForm{
   email:string;
@@ -11,6 +14,12 @@ export interface IRegisterForm{
   name: string;
   email: string;
   password: string;
+}
+export interface IResetPwForm{
+  email: string;
+}
+export interface IEnterCodeForm{
+  code: string;
 }
 
 @Component({
@@ -25,14 +34,20 @@ export class LoginformComponent {
 @Output() googleLogin = new EventEmitter<void>();
 @Output() login = new EventEmitter<ILoginForm>();
 @Output() register = new EventEmitter<IRegisterForm>();
+@Output() resetPw = new EventEmitter<IResetPwForm>();
+@Output() enterCode = new EventEmitter<IEnterCodeForm>();
 
-private formBuilder = inject(FormBuilder)
+private formBuilder = inject(FormBuilder);
+private route = inject(ActivatedRoute);
 
-protected formTemplate: 'login' | 'register' = 'login'; 
+protected formTemplate: 'login' | 'register' | 'resetPw' | 'enterCode' = 'login'; 
 protected registrationForm: FormGroup;
 protected loginForm: FormGroup;
+protected resetPwForm: FormGroup;
+protected enterCodeForm: FormGroup;
 
 constructor(){
+
   this.registrationForm = this.formBuilder.group({
     name: ['', Validators.required],           
     email: ['', [Validators.required, Validators.email]], 
@@ -44,11 +59,26 @@ constructor(){
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   })
+
+  this.resetPwForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  })
+
+  this.enterCodeForm = this.formBuilder.group({
+    code: ['', [Validators.required]],
+  })
 }
 
 protected switchTemplate(){
   if(this.formTemplate === 'login'){
     this.formTemplate = 'register'
+  }else{
+    this.formTemplate = 'login'
+  }
+}
+protected switchTemplatePw(){
+  if(this.formTemplate === 'login'){
+    this.formTemplate = 'resetPw'
   }else{
     this.formTemplate = 'login'
   }
@@ -71,8 +101,18 @@ protected onRegistration(){
 
 }
 
-protected save(){
-  
+protected resetPassword(){
+  const email = this.resetPwForm.value;
+  if(email){
+    this.resetPw.emit(email);
+  }
+}
+
+protected checkCode(){
+  const code = this.enterCodeForm.value;
+  if(code){
+    this.resetPw.emit(code);
+  }
 }
 
 private passwordMatchValidator(formGroup: FormGroup) {
